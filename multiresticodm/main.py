263a03b8561b21e5984b36d9687b4948bbefc231
YannisZa <yzachos@gmail.com>
2024-02-09 09:33:34 +0000

    added reproduce command

--- multiresticodm/main.py
+++ multiresticodm/main.py
@@ -5,6 +5,7 @@ import json
 import click
 import torch
 import psutil
+import subprocess
 
 from multiresticodm.config import Config
 from multiresticodm.utils.logger_class import *
@@ -494,6 +495,8 @@ _output_options = [
     click.option('--evaluate','-e', multiple = True, type=(click.STRING, click.STRING), required = False, default=[], 
                 callback = list_of_lists, help = f'''Evaluates expressions for one or more datasets. 
                 First argument is the name of the evaluation. Second is the evaluation expression'''),
+    click.option('--folder_kwargs','-fa', multiple = True, type = click.STRING, required = False, default = [{}], 
+                callback = unstringify_callback, help = f'''Expression evaluation keyword arguments by output folder.'''),
     click.option('--evaluation_kwargs','-ea', multiple = True, type = click.STRING, required = False, default = None, 
                 callback = evaluate_kwargs_callback, help = f'''Expression evaluation keyword arguments.'''),
     click.option('--evaluation_library','-el', multiple = True, type = click.STRING, required = False, default = ["np"], callback = list_of_str, help = f'''Expression evaluation libraries that needs to be loaded before applying evaluation'''),
@@ -584,6 +587,8 @@ def plot_coordinate_options(func):
               type = click.STRING, help = f'Sets metadata key(s) to determine figure element opacity/visibility/transparency by.')
 @click.option('--hatch_opacity', '-hchop', default='1.0', show_default = False,
               type = click.STRING, help = f'Sets metadata key(s) to determine hatch pattern opacity/visibility/transparency by.')
+@click.option('--hatch_linewidth', '-hlw', default='1.0', show_default = False,
+              type = click.STRING, help = f'Sets metadata key(s) to determine hatch pattern line width by.')
 @click.option('--hatch', '-hch', default='', show_default = False,
               type = click.STRING, help = f'Sets metadata key(s) to determine figure element marker texture by.')
 @click.option('--zorder', '-or', default=[(None,None)], show_default = False, multiple = True, callback = to_list,
@@ -599,9 +604,11 @@ def plot_coordinate_options(func):
               type=click.Choice(LEGEND_LOCATIONS), help = f'Sets the legend locations.')
 @click.option('--legend_cols', '-lc', default=1, show_default = True,
               type=click.IntRange(min=1), help = f"Sets the legend's number of columns.")
-@click.option('--legend_col_spacing', '-lcs', default=1.0, show_default = True,
+@click.option('--legend_cols', '-lc', default=1, show_default = True,
+              type=click.IntRange(min=1), help = f"Sets the legend's number of columns.")
+@click.option('--legend_col_spacing', '-lcs', default=None, show_default = True,
               type=click.FloatRange(min=0.0), help = f"Sets the legend's spacing between columns.")
-@click.option('--legend_pad', '-lp', default=0.1, show_default = True,
+@click.option('--legend_pad', '-lp', default=None, show_default = True,
               type=click.FloatRange(min=0.0), help = f"Sets the legend's spacing between handlers and labels.")
 @click.option('--bbox_to_anchor', '-bbta', default=None, show_default = True, multiple = True, callback = to_list,
               type=click.FLOAT, help = f'Box that is used to position the legend in conjunction with legend_location.')
@@ -683,6 +690,7 @@ def plot(
         metric,
         metric_args,
         evaluate,
+        folder_kwargs,
         evaluation_kwargs,
         evaluation_library,
         dates,
@@ -713,6 +721,7 @@ def plot(
         line_width,
         opacity,
         hatch_opacity,
+        hatch_linewidth,
         hatch,
         zorder,
         annotate,
@@ -830,7 +839,8 @@ def plot(
         plot_view = plot_view,
         outputs_directories = list(directories),
         settings = settings,
-        logger = logger
+        logger = logger,
+        pop = False
     )
 
     logger.info('Done')
@@ -922,7 +932,95 @@ def summarise(
 
     logger.info('Done')
 
+@cli.command(name='reproduce',context_settings = dict(
+    ignore_unknown_options = False,
+    allow_extra_args = False
+))
+@click.argument('figure', type = click.Choice(['figure1','figure2','figure3','figure4']), default = None)
+def reproduce(figure):
+    """
+    Reproduce figures in the paper.
+    """
+    if figure == 'figure1':
+        subprocess.check_output([
+            "multiresticodm", "plot", "2d", "line", "--y_shade", "--y_group", "type", "-y", "table_density", "-x", "density_eval_points", 
+            "-dn", "cambridge_work_commuter_lsoas_to_msoas/exp1", "-et", "JointTableSIM_MCMC", "-et", "NonJointTableSIM_NN", "-et", "JointTableSIM_NN",
+            "-el", "np", "-el", "ProbabilityUtils", "-el", "xr", 
+            "-e", "table_density", "xr.apply_ufunc(kernel_density,table_like_loss.groupby('sweep'),kwargs={'x':xs,'bandwidth':bandwidth},exclude_dims=set(['id']),input_core_dims=[['id']],output_core_dims=[['id']])", 
+            "-e", "density_eval_points", "xr.DataArray(xs)", 
+            "-e", "table_density_height", "np.nanmax(table_density)", 
+            "-fa", "{'xs':np.linspace(3.6,3.7,1000),'bandwidth':0.25}", 
+            "-fa", "{'xs':np.linspace(3.6,3.7,1000),'bandwidth':0.25}", 
+            "-fa", "{'xs':np.linspace(2.4,4.2,1000),'bandwidth':0.25}", 
+            "-ea", "table", "-ea", "intensity", 
+            "-ea", "kernel_density=ProbabilityUtils.kernel_density", 
+            "-ea", "table_lossfn=outputs.ct_mcmc.table_loss", 
+            "-ea", "table_like_loss=table_lossfn(table/table.sum(['origin','destination']),np.log(intensity/intensity.sum(['origin','destination'])))", 
+            "-btt", "iter", "100", "100", "1000", 
+            "-cs", "da.loss_name.isin([str(['dest_attraction_ts_likelihood_loss']),str(['dest_attraction_ts_likelihood_loss', 'table_likelihood_loss'])])", 
+            "-cs", "~da.title.isin(['_unconstrained','_total_intensity_row_table_constrained'])", 
+            "-k", "sigma", "-k", "type", "-k", "title", 
+            "-ft", 'figure1/figure1_table_like_loss_kernel_density', "-ff", "ps", 
+            "-hchv", "0.5", "-hch", "sigma", "-c", "title", "-op", "1.0", "-msz", "1", "-l", "title", "-l", "sigma", "-or", "asc", "table_density_height", 
+            "-xlab", '$\mathcal{L}\left(\mytable,\myintensity\\right)$', "-ylab", "Kernel density",
+            "-fs", "6", "10", "-lls", "11", "-lp", "0.5", "-la", "2", "0", "-lc", "2", "-loc", 'lower center', "-bbta", "0.5", "-bbta", "-1.1", 
+            "-ylr", "90", "-xts", "12", "12", "-yts", "12", "12", "-xtp", "0", "0", "-ytp", "0", "0", "-ylp", "2", "-xlp", "1",
+            "-xlim", "2.8", "3.7", "-xlim", "3.61", "3.67", "-xlim", "3.61", "3.67", "-ylim", "0", "10", "-ylim", "0", "300", "-ylim", "0", "500", "-hlw", "0.2"
+        ])
+    elif figure == 'figure2':
+        subprocess.check_output([
+            "multiresticodm", "plot", "2d", "scatter", "-y", "table_srmse", "-x", "type", "-x", "end", "--x_discrete", 
+            "-dn", "cambridge_work_commuter_lsoas_to_msoas/exp1", 
+            "-et", "JointTableSIM_MCMC", "-et", "JointTableSIM_NN", "-et", "NonJointTableSIM_NN", 
+            "-el", "np", "-el", "MathUtils", "-el", "MiscUtils", "-el", "xr", 
+            "-e", "table_coverage_probability", "xr.apply_ufunc(roundint, 100*table_coverage.mean(['origin','destination'])).astype('int32')", 
+            "-e", "table_coverage_probability_size", "xr.apply_ufunc(lambda, x:, np.exp(8*x-2), table_coverage.mean(['origin','destination']))", 
+            "-e", "table_srmse", "apply_and_combine(table,functions=srmse_functions,fixed_kwargs=fixed_kwargs,isolated_sweeped_kwargs=isolated_sweeped_kwargs)", 
+            "-ea", "table", 
+            "-ea", "endings=[10000,20000,40000,60000,80000,100000]", "-ea", "region_mass=[0.99]", 
+            "-ea", "ground_truth=outputs.inputs.data.ground_truth_table", 
+            "-ea", "apply_and_combine=MiscUtils.xr_apply_and_combine_wrapper", "-ea", "srmse=MathUtils.srmse", "-ea", "islice=MiscUtils.xr_islice", "-ea", "coverage_probability=MathUtils.coverage_probability", "-ea", "sample_mean=MathUtils.sample_mean", "-ea", "roundint=MathUtils.roundint", 
+            "-ea", "cp_functions=[{'islice':{'callable':islice}},{'coverage_probability':{'callable':coverage_probability}}]", 
+            "-ea", "srmse_functions=[{'islice':{'callable':islice}},{'sample_mean':{'callable':sample_mean}},{'srmse':{'callable':srmse}}]", 
+            "-ea", "fixed_kwargs={'islice':{'dim':'id'},'coverage_probability':{'ground_truth':ground_truth},'srmse':{'ground_truth':ground_truth},'sample_mean':{'dim':[str('id')]}}", 
+            "-ea", "isolated_sweeped_kwargs={'end':endings,'region_mass':region_mass}", 
+            "-ea", "table_coverage=apply_and_combine(table,functions=cp_functions,fixed_kwargs=fixed_kwargs,isolated_sweeped_kwargs=isolated_sweeped_kwargs)", 
+            "-k", "sigma", "-k", "type", "-k", "name", "-k", "title", 
+            "-cs", "da.loss_name.isin([str(['dest_attraction_ts_likelihood_loss']),str(['dest_attraction_ts_likelihood_loss', table_likelihood_loss'])])", 
+            "-cs", "~da.title.isin(['_unconstrained','_total_constrained','_total_intensity_row_table_constrained'])", 
+            "-c", "title", "-op", "1.0", "-mrkr", "sigma", "-msz", "table_coverage_probability_size", "-l", "title", "-l", "sigma", "-or", "asc", "table_coverage_probability_size", 
+            "-ft", "figure2/cumulative_srmse_and_cp_by_method", "-ylab", "SRMSE$\left(\mathbb{E}\left[\mytable^{(1:N)}\\right],\groundtruthtable\\right)$", "-xlab", "Method, $N$", 
+            "-la", "0", "0", "-lc", "2", "-loc", "upper center", "-bbta", "0.5", "-bbta", "1.35", "-lls", "14", "-ylr", "90", "-xls", "20", "-yls", "20", "-yts", "18", "18", "-xts", "12", "16", 
+            "-xtp", "0", "102", "-ytl", "0.0", "0.2", "-xtl", "1", "1", "-xtl", "2", "3", "-xlim", "0", "19", "-ylim", "0", "1.8", "-xtr", "75", "0"
+        ])
+    elif figure == 'figure3':
+        subprocess.check_output([
+            "multiresticodm", "plot", "2d", "scatter", "-y", "table_srmse", "-x", "type", "-x", "N&ensemble_size", "--x_discrete", "-gb", "seed", 
+            "-dn", "cambridge_work_commuter_lsoas_to_msoas/exp2",
+            "-et", "NonJointTableSIM_NN", "-et", "JointTableSIM_NN",
+            "-el", "np", "-el", "MathUtils", "-el", "MiscUtils", "-el", "xr",
+            "-e", "table_coverage_probability_size", "xr.apply_ufunc(lambda x: np.exp(6*x), table_coverage.mean(['origin','destination']))",
+            "-e", "table_srmse", "srmse(prediction=mean_table,ground_truth=ground_truth)", 
+            "-e", "ensemble_size", "table_srmse.copy(data=[len(table.unstack('id').coords['seed'].values)])", 
+            "-ea", "table", 
+            "-ea", "mean_table=table.mean(['id'])", 
+            "-ea", "ground_truth=outputs.inputs.data.ground_truth_table", 
+            "-ea", "srmse=MathUtils.srmse", "-ea", "coverage_probability=MathUtils.coverage_probability", "-ea", "roundint=MathUtils.roundint", 
+            "-ea", "table_coverage=coverage_probability(prediction=table,ground_truth=ground_truth)", 
+            "-cs", "da.loss_name.isin([str(['dest_attraction_ts_likelihood_loss']),str(['dest_attraction_ts_likelihood_loss', 'table_likelihood_loss'])])", 
+            "-cs", "~da.title.isin(['_unconstrained','_total_intensity_row_table_constrained'])", 
+            "-k", "sigma", "-k", "type", "-k", "name", "-k", "title", "-k", "N", 
+            "-mrkr", "sigma", "-c", "title", "-msz", "table_coverage_probability_size", "-op", "1.0", "-or", "asc", "table_coverage_probability_size", "-l", "sigma", "-l", "title", 
+            "-fs", "10", "10", "-ff", "ps", "-ft", "figure3/exploration_exploitation_tradeoff_srmse_cp_vs_method_epoch_seed", 
+            "-xlab", "Method, ($N$, $E$)", "-ylab", "SRMSE$\left(\mathbb{E}[\mytable^{(1:N)}],\mytable^{\mathcal{D}}\\right)$", 
+            "-ylim", "0.0", "3.2", "-ylr", "90", "-xtp", "0", "80", "-ytl", "0.0", "0.2", "-ytl", "0.0", "0.0", "-xtl", "5", "8", "-xtl", "9", "16", "-yts", "18", "18", "-xts", "18", "18", "-xts", "18", "18", 
+            "-xtr", "70", "0", "-xls", "20", "-yls", "20", "-xlim", "0", "111", "-la", "0", "0", "-lls", "14", "-loc", "upper center", "-bbta", "0.45", "-bbta", "1.3", "-btta", "0.4", "-btta", "1.0", "-lc", "3", "-lp", "0.01", "-lcs", "0.1"
+        ])
+    elif figure == 'figure4':
+        raise NotImplementedError
+
 if __name__ == '__main__':
     # run()
     plot()
     # summarise()
+
