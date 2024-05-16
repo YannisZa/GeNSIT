0c8d5f21d74ae058ce448c10d931647d767b35b2
YannisZa <yzachos@gmail.com>
2024-05-16 15:40:05 +0100

    minor refactoring

--- gensit/learning_models/RandomForest.py
+++ gensit/learning_models/RandomForest.py
@@ -1,6 +1,7 @@
 import sys
 import json
 import optuna
+import numpy as np
 
 from copy import deepcopy
 from sklearn.exceptions import NotFittedError
@@ -13,7 +14,6 @@ from gensit.utils.misc_utils import setup_logger
 
 
 DEFAULT_HYPERPARAMS = {
-    'n_estimators': 100,
     'max_depth': None,
     'min_samples_split': 10,
     'min_samples_leaf': 3,
@@ -29,6 +29,7 @@ class RF_Model(object):
         *,
         config: Config,
         trial: optuna.trial,
+        logger,
         **kwargs
     ):
         # Setup logger
@@ -37,7 +38,7 @@ class RF_Model(object):
             __name__+kwargs.get('instance',''),
             console_level = level,
             
-        ) if kwargs.get('logger',None) is None else kwargs['logger']
+        ) if kwargs.get('logger',None) is None else logger
         # Update logger level
         self.logger.setLevels( console_level = level )
         
@@ -55,7 +56,8 @@ class RF_Model(object):
         # Initialise Random Forest Regressor
         self.random_forest = RandomForestRegressor(
             **{k.replace('rf_',''):v for k,v in self.hyperparams.items()},
-            n_jobs = self.config['inputs'].get('n_threads',1)
+            n_jobs = self.config['inputs'].get('n_threads',1),
+            **kwargs
         )
 
     def update_hyperparameters(self):
@@ -63,13 +65,14 @@ class RF_Model(object):
         self.hyperparams = {}
         if self.trial is not None:
             OPTUNA_HYPERPARAMS = {
-                'n_estimators': self.trial.suggest_int('n_estimators', 10, 100, step = 50),
+                # 'n_estimators': self.trial.suggest_int('n_estimators', 10, 100, step = 50),
                 'max_depth': self.trial.suggest_int('max_depth', 1, 12, step = 1),
                 'min_samples_split': self.trial.suggest_int('min_samples_split', 2, 100, step = 10),
                 'min_samples_leaf': self.trial.suggest_int('min_samples_leaf', 1, 50, step = 5),
                 'oob_score':self.trial.suggest_categorical('oob_score',[True,False])
             } 
         
+        self.hyperparams['n_estimators'] = self.config['training']['N']
         for pname in DEFAULT_HYPERPARAMS.keys():
             if self.trial is not None and pname in OPTUNA_HYPERPARAMS:
                 self.hyperparams[pname] = OPTUNA_HYPERPARAMS[pname]
@@ -83,29 +86,11 @@ class RF_Model(object):
                 self.config[self.model_type]['hyperparameters'][(self.model_prefix+pname)] = self.hyperparams[pname]
         
 
-
     def train(self, train_x, train_y, **kwargs):
-        # Train
-        return self.random_forest.fit(
-            X = train_x, 
-            y = train_y
-        )
+        self.random_forest.fit(train_x, train_y, **kwargs)
     
-    def predict(self, test_x):
-        return self.random_forest.predict(test_x)
-    
-    def run_single(self,train_x,train_y,test_x):
-        # Train
-        self.train(
-            train_x = train_x,
-            train_y = train_y
-        )
-        
-        # Test (predict)
-        intensity = self.predict(
-            test_x = test_x
-        )
-        return intensity
+    def predict_single(self, test_x, estimator_index):
+        return self.random_forest.estimators_[estimator_index].predict(test_x)
         
     def __repr__(self):
         return self.random_forest.__repr__()
